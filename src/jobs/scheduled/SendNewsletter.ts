be7022d62f340b256f654fe45090e90a8a805368
João Alves <joao.alves@ecosis.com.br>
2023-02-02 14:03:45 -0300

    :recycle: Remove Newsletter controller

--- src/jobs/scheduled/SendNewsletter.ts
+++ src/jobs/scheduled/SendNewsletter.ts
@@ -1,11 +1,16 @@
 import schedule from 'node-schedule'
-import SendNewsletterToContacts from '../../controllers/SendNewsletterToContacts'
+import NewsletterMail from '../NewsletterMail'
+
+const contacts = [
+    { name: 'Fulano 01', email: 'fulano01@gmail.com' },
+]
 
 class SendNewsletter {
     key = 'SendNewsletter'
     rule = new schedule.RecurrenceRule()
 
     constructor() {
+        
         // On weekdays at 7:30AM
         this.rule.dayOfWeek = [new schedule.Range(1, 5)]
         this.rule.hour = 7
@@ -13,8 +18,12 @@ class SendNewsletter {
         this.rule.tz = 'UTC-03:00'
     }
 
-    handle() {
-        SendNewsletterToContacts.handle()
+    async handle() {
+        await Promise.all(
+            contacts.map((contact) => {
+                return NewsletterMail.handle({ contact })
+            })
+        )
     }
 }
 
