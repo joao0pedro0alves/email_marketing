083c5592992aefa79ba409a85249a2fe545f9afe
João Alves <joao.alves@ecosis.com.br>
2023-02-03 12:28:21 -0300

    :sparkles: Create trigger newsletter mail template

--- src/jobs/NewsletterMail.ts
+++ src/jobs/NewsletterMail.ts
@@ -1,10 +1,11 @@
 import path from 'path'
 import fs from 'fs'
 import handlebars from 'handlebars'
-import { Options } from 'nodemailer/lib/mailer'
+import { Attachment, Options } from 'nodemailer/lib/mailer'
 
 import { Message } from '@prisma/client'
 import { transporter } from '../lib/mailer'
+import { randomIntFromInterval } from '../utils/randomIntFromInterval'
 
 interface NewsletterMailParams extends Options {
     message: Message
@@ -18,7 +19,9 @@ class NewsletterMail {
     key = 'NewsletterMail'
 
     async handle({ message, contact, ...options }: NewsletterMailParams) {
-        const filePath = path.join(__dirname, '../emails/template.html')
+        const filePath = path.join(__dirname, '../emails/newsletter.html')
+        const attachPath = path.join(__dirname, '../emails/assets')
+
         const source = fs.readFileSync(filePath, 'utf-8').toString()
         const template = handlebars.compile(source)
 
@@ -30,11 +33,36 @@ class NewsletterMail {
 
         const htmlToSend = template(replacements)
 
+        const randomBannerFilename = `banner-${randomIntFromInterval(1, 3)}.png`
+        const attachments: Attachment[] = [
+            {
+                filename: randomBannerFilename,
+                path: `${attachPath}/${randomBannerFilename}`,
+                cid: "@banner"
+            },
+            {
+                filename: 'stripo-logo.png',
+                path: `${attachPath}/stripo-logo.png`,
+                cid: "@stripologo"
+            },
+            {
+                filename: 'icon-header.png',
+                path: `${attachPath}/icon-header.png`,
+                cid: "@iconheader"
+            },
+            {
+                filename: 'icon-footer.png',
+                path: `${attachPath}/icon-footer.png`,
+                cid: "@iconfooter"
+            }
+        ]
+
         transporter.sendMail({
             from: process.env.MAIL_SENDER,
             to: contact.email,
             subject: message.title,
             html: htmlToSend,
+            attachments,
             ...options,
         })
     }
