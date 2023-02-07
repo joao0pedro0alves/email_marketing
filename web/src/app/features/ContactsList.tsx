7cdbd1eb7c930f5979bcb931e6ed52e80ffac5dc
João Alves <joao.alves@ecosis.com.br>
2023-02-07 11:34:56 -0300

    :lipstick: Create features

---
+++ web/src/app/features/ContactsList.tsx
@@ -0,0 +1,9 @@
+import { SelectField } from '../components/SelectField'
+
+export function ContactsList() {
+    return (
+        <div className="mt-2">
+            <SelectField />
+        </div>
+    )
+}
